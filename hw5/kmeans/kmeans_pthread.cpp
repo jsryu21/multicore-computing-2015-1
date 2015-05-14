63017c8ac3096678ad07a666d2b28f21ac0f4438
jsryu21 <jsryu21@snu.ac.kr>
2015-05-14 20:36:38 +0900

    size_t modified.

---
+++ hw5/kmeans/kmeans_pthread.cpp
@@ -0,0 +1,217 @@
+
+#include "kmeans.h"
+#include <cstdlib>
+#include <cfloat>
+#include <pthread.h>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+class ILock {
+    public:
+        virtual void Lock() = 0;
+        virtual void Unlock() = 0;
+};
+
+class TTASlock : public ILock {
+    public:
+        TTASlock()
+            : state(false)
+        {
+        }
+        void Lock()
+        {
+            while (true)
+            {
+                while (state) {}
+                if (__sync_lock_test_and_set(&state, true) == false)
+                {
+                    return;
+                }
+            }
+        }
+        void Unlock()
+        {
+            __sync_lock_release(&state);
+        }
+    private:
+        bool state;
+};
+
+struct AssignThreadData {
+    AssignThreadData(int class_n, const Point* centroids, const Point* data, int* partitioned);
+    int class_n;
+    const Point* centroids;
+    const Point* data;
+    int* partitioned;
+    int data_start_index;
+    int data_end_index;
+};
+
+AssignThreadData::AssignThreadData(int class_n, const Point* centroids, const Point* data, int* partitioned) {
+    this->class_n = class_n;
+    this->centroids = centroids;
+    this->data = data;
+    this->partitioned = partitioned;
+}
+
+struct UpdateThreadData {
+    UpdateThreadData(const Point* data, const int* partitioned, ILock& lock_, int class_n, Point* centroids, std::vector< int >& count_);
+    const Point* data;
+    const int* partitioned;
+    ILock& lock;
+    int class_n;
+    Point* centroids;
+    std::vector< int >& count;
+    int data_start_index;
+    int data_end_index;
+    std::vector< Point >* temp_centroids;
+    std::vector< int >* temp_count;
+};
+
+UpdateThreadData::UpdateThreadData(const Point* data, const int* partitioned, ILock& lock_, int class_n, Point* centroids, std::vector< int >& count_) : lock(lock_), count(count_)
+{
+    this->data = data;
+    this->partitioned = partitioned;
+    this->lock = lock;
+    this->class_n = class_n;
+    this->centroids = centroids;
+}
+
+void* AssignmentWork(void* threadarg) {
+    AssignThreadData* my_data = static_cast< AssignThreadData* >(threadarg);
+    Point t(0.f, 0.f);
+    int class_n = my_data->class_n;
+    const Point* centroids = my_data->centroids;
+    const Point* data = my_data->data;
+    int* partitioned = my_data->partitioned;
+    int data_start_index = my_data->data_start_index;
+    int data_end_index = my_data->data_end_index;
+
+    for (int data_i = data_start_index; data_i < data_end_index; ++data_i) {
+        float min_dist = DBL_MAX;
+        for (int class_i = 0; class_i < class_n; class_i++) {
+            t.x = data[data_i].x - centroids[class_i].x;
+            t.y = data[data_i].y - centroids[class_i].y;
+            float dist = t.x * t.x + t.y * t.y;
+            if (dist < min_dist) {
+                partitioned[data_i] = class_i;
+                min_dist = dist;
+            }
+        }
+    }
+
+    pthread_exit(static_cast< void* >(my_data));
+}
+
+void* UpdateWork(void* threadarg) {
+    UpdateThreadData* my_data = static_cast< UpdateThreadData* >(threadarg);
+    const Point* data = my_data->data;
+    const int* partitioned = my_data->partitioned;
+    ILock& lock = my_data->lock;
+    int class_n = my_data->class_n;
+    Point* centroids = my_data->centroids;
+    std::vector< int >& count = my_data->count;
+    int data_start_index = my_data->data_start_index;
+    int data_end_index = my_data->data_end_index;
+    std::vector< Point >* temp_centroids = my_data->temp_centroids;
+    std::vector< int >* temp_count = my_data->temp_count;
+
+    // Sum up and count data for each class
+    for (int data_i = data_start_index; data_i < data_end_index; data_i++) {
+        (*temp_centroids)[partitioned[data_i]].x += data[data_i].x;
+        (*temp_centroids)[partitioned[data_i]].y += data[data_i].y;
+        (*temp_count)[partitioned[data_i]]++;
+    }
+
+    lock.Lock();
+    for (int class_i = 0; class_i < class_n; ++class_i) {
+        centroids[class_i].x += (*temp_centroids)[class_i].x;
+        centroids[class_i].y += (*temp_centroids)[class_i].y;
+        count[class_i] += (*temp_count)[class_i];
+    }
+    lock.Unlock();
+
+    pthread_exit(static_cast< void* >(my_data));
+}
+
+void kmeans(int iteration_n, int class_n, int data_n, Point* centroids, Point* data, int* partitioned, int num_threads)
+{
+    // Count number of data in each class
+    std::vector< int > count(class_n);
+    std::vector< AssignThreadData > assignThreadDatas(num_threads, AssignThreadData(class_n, centroids, data, partitioned));
+    int data_gap = static_cast< int >(std::ceil(data_n / num_threads));
+    for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
+        AssignThreadData& my_data = assignThreadDatas[thread_id];
+        my_data.data_start_index = data_gap * thread_id;
+        my_data.data_end_index = std::min(my_data.data_start_index + data_gap, data_n);
+    }
+    TTASlock lock;
+    std::vector< std::vector< Point > > tempCentroids(num_threads, std::vector< Point >(class_n, Point(0.f, 0.f)));
+    std::vector< std::vector< int > > tempCount(num_threads, std::vector< int >(class_n));
+    std::vector< UpdateThreadData > updateThreadDatas(num_threads, UpdateThreadData(data, partitioned, lock, class_n, centroids, count));
+    for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
+        UpdateThreadData& my_data = updateThreadDatas[thread_id];
+        my_data.data_start_index = data_gap * thread_id;
+        my_data.data_end_index = std::min(my_data.data_start_index + data_gap, data_n);
+        my_data.temp_centroids = &tempCentroids[thread_id];
+        my_data.temp_count = &tempCount[thread_id];
+    }
+    pthread_t thread[num_threads];
+    pthread_attr_t attr;
+    void* status;
+
+    /* Initialize and set thread detached attribute */
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    // Iterate through number of interations
+    for (int i = 0; i < iteration_n; i++) {
+        // Assignment step
+        for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
+            int rc = pthread_create(&thread[thread_id], &attr, AssignmentWork, static_cast< void* >(&assignThreadDatas[thread_id]));
+            if (rc) {
+                std::cout << "ERROR; return code from pthread_create() is " << rc << std::endl;
+                exit(-1);
+            }
+        }
+
+        pthread_attr_destroy(&attr);
+        for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
+            int rc = pthread_join(thread[thread_id], &status);
+            if (rc) {
+                std::cout << "ERROR; return code from pthread_join() is " << rc << std::endl;
+                exit(-1);
+            }
+        }
+
+        // Update step
+        // Clear sum buffer and class count
+        std::fill(centroids, centroids + class_n, Point(0.f, 0.f));
+        std::fill(count.begin(), count.end(), 0);
+        std::fill(tempCentroids.begin(), tempCentroids.end(), std::vector< Point >(class_n, Point(0.f, 0.f)));
+        std::fill(tempCount.begin(), tempCount.end(), std::vector< int >(class_n, 0));
+
+        for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
+            int rc = pthread_create(&thread[thread_id], &attr, UpdateWork, static_cast< void* >(&updateThreadDatas[thread_id]));
+            if (rc) {
+                std::cout << "ERROR; return code from pthread_create() is " << rc << std::endl;
+                exit(-1);
+            }
+        }
+
+        pthread_attr_destroy(&attr);
+        for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
+            int rc = pthread_join(thread[thread_id], &status);
+            if (rc) {
+                std::cout << "ERROR; return code from pthread_join() is " << rc << std::endl;
+                exit(-1);
+            }
+        }
+        // Divide the sum with number of class for mean point
+        for (int class_i = 0; class_i < class_n; class_i++) {
+            centroids[class_i].x /= count[class_i];
+            centroids[class_i].y /= count[class_i];
+        }
+    }
+}
---
+++ hw5/matmul/mat_mul.c
@@ -0,0 +1,211 @@
+#include <CL/cl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <math.h>
+#include "timers.h"
+#include <stdbool.h>
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define SIZE_I 100000
+#define SIZE_J 100000
+#define SIZE_K 100000
+#define PRECISION 0.00001
+
+const char* kernel_src = "__kernel void matrixmul(__global float* C, __global float* A, __global float* B, int wA, int wB) {"
+"    int i = get_global_id(0);"
+"    int j = get_global_id(1);"
+"   if (i == 0 && j == 0) {"
+"       printf(\"%d %d\n\", i, j);"
+"   }"
+"}";
+
+bool print_matrix = false;
+bool validation = false;
+
+/************************** DO NOT TOUCH BELOW HERE ******************************/
+
+void check_mat_mul(float* matrixC, int widthC, int heightC, float* matrixA, int widthA, int heightA, float* matrixB, int widthB, int heightB) {
+    int i, j, k;
+    float sum;
+    bool validated = true;
+
+    printf("Validating the result..\n");
+
+    if (widthA != heightB) {
+        validated = false;
+    } else if (heightA != heightC) {
+        validated = false;
+    } else if (widthB != widthC) {
+        validated = false;
+    } else {
+        // C = AB
+        for (i = 0; i < heightA; ++i) {
+            size_t rowIndexA = (size_t)i * widthA;
+            size_t rowIndexC = (size_t)i * widthC;
+            for (j = 0; j < widthB; ++j) {
+                sum = 0.f;
+                for (k = 0; k < widthA; ++k) {
+                    size_t indexA = rowIndexA + k;
+                    size_t indexB = (size_t)k * widthB + j;
+                    sum += matrixA[indexA] * matrixB[indexB];
+                }
+                size_t indexC = rowIndexC + j;
+                float value = matrixC[indexC];
+                if (fabs(value - sum) > PRECISION) {
+                    //printf("c[%d][%d] is differ(value=%lf correct_value=%lf)!!\n", i, j, value, sum);
+                    validated = false;
+                }
+            }
+        }
+    }
+
+    printf("Validation : ");
+    if(validated) {
+        printf("SUCCESSFUL.\n");
+    } else {
+        printf("FAILED.\n");
+    }
+}
+
+void print_mat(float* matrix, int width, int height) {
+    int i, j;
+    for (i = 0; i < height; ++i) {
+        for (j = 0; j < width; ++j) {
+            printf("%8.2lf ", matrix[i * height + j]);
+        }
+        printf("\n");
+    }
+}
+
+void print_help(const char* prog_name) {
+    printf("Usage: %s [-pvht]\n", prog_name );
+    printf("\n");
+    printf("OPTIONS\n");
+    printf("  -p : print matrix data.\n");
+    printf("  -v : validate matrix multiplication.\n");
+    printf("  -h : print this page.\n");
+}
+
+void parse_opt(int argc, char** argv) {
+    int opt;
+    while ((opt = getopt(argc, argv, "pvh")) != -1 ) {
+        switch(opt) {
+            case 'p':
+                // print matrix data.
+                print_matrix = 1;
+                break;
+
+            case 'v':
+                // validation
+                validation = 1;
+                break;
+
+            case 'h':
+            default:
+                print_help(argv[0]);
+                exit(0);
+                break;
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    parse_opt(argc, argv);
+    // CL code
+    cl_platform_id platform;
+    cl_device_id device;
+    cl_context context;
+    cl_command_queue command_queue;
+    cl_program program;
+    cl_kernel kernel;
+    cl_mem bufferA;
+    cl_mem bufferB;
+    cl_mem bufferC;
+    float* hostA;
+    float* hostB;
+    float* hostC;
+    int wA, hA, wB, hB, wC, hC;
+    size_t sizeA, sizeB, sizeC;
+    hA = hC = SIZE_I;
+    wB = wC = SIZE_J;
+    wA = hB = SIZE_K;
+    sizeA = (size_t)hA * wA * sizeof(float);
+    sizeB = (size_t)hB * wB * sizeof(float);
+    sizeC = (size_t)hC * wC * sizeof(float);
+    hostA = (float*)malloc(sizeA);
+    hostB = (float*)malloc(sizeB);
+    hostC = (float*)malloc(sizeC);
+    // assign matrix
+    int i, j;
+    for (i = 0; i < hA; ++i) {
+        size_t rowIndex = (size_t)wA * i;
+        for (j = 0; j < wA; ++j) {
+            size_t index = rowIndex + j;
+            if (index % 10000 == 0) {
+                printf("%zu\n", index);
+            }
+            hostA[index] = 1;
+        }
+    }
+    for (i = 0; i < hB; ++i) {
+        size_t rowIndex = (size_t)wB * i;
+        for (j = 0; j < wB; ++j) {
+            size_t index = rowIndex + j;
+            if (index % 10000 == 0) {
+                printf("%zu\n", index);
+            }
+            hostB[index] = 2;
+        }
+    }
+    clGetPlatformIDs(1, &platform, NULL);
+    clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+    context = clCreateContext(0, 1, &device, NULL, NULL, NULL);
+    command_queue = clCreateCommandQueue(context, device, 0, NULL);
+    bufferA = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeA, NULL, NULL);
+    bufferB = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeB, NULL, NULL);
+    bufferC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeC, NULL, NULL);
+    size_t kernel_src_len = (size_t)strlen(kernel_src);
+    program = clCreateProgramWithSource(context, 1, (const char**)&kernel_src, &kernel_src_len, NULL);
+    clBuildProgram(program, 1, &device, NULL, NULL, NULL);
+    kernel = clCreateKernel(program, "matrixmul", NULL);
+    clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&bufferC);
+    clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&bufferA);
+    clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&bufferB);
+    clSetKernelArg(kernel, 3, sizeof(cl_int), (void*)&wA);
+    clSetKernelArg(kernel, 4, sizeof(cl_int), (void*)&wB);
+    timer_start(1);
+    // prepare the input data
+    clEnqueueWriteBuffer(command_queue, bufferA, CL_FALSE, 0, sizeA, hostA, 0, NULL, NULL);
+    clEnqueueWriteBuffer(command_queue, bufferB, CL_FALSE, 0, sizeB, hostB, 0, NULL, NULL);
+    size_t global[2] = {wC, hC};
+    size_t local[2] = {SIZE_I, SIZE_J};
+    // launch the kernel
+    clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, global, local, 0, NULL, NULL);
+    clEnqueueReadBuffer(command_queue, bufferC, CL_TRUE, 0, sizeC, hostC, 0, NULL, NULL);
+    timer_stop(1);
+
+    printf("Time elapsed : %lf sec\n", timer_read(1));
+
+    if(validation) {
+        check_mat_mul(hostC, wC, hC, hostA, wA, hA, hostB, wB, wB);
+    }
+
+    if(print_matrix) {
+        printf("MATRIX A: \n");
+        print_mat(hostA, wA, hA);
+
+        printf("MATRIX B: \n");
+        print_mat(hostB, wB, hB);
+
+        printf("MATRIX C: \n");
+        print_mat(hostC, wC, hC);
+    }
+
+    free(hostC);
+    free(hostB);
+    free(hostA);
+    return 0;
+}
